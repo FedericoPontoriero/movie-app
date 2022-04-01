89e90a9c3dd6d8565017ffc7e3f7e5fac3c47b72
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-04-01 15:14:16 -0300

    Added data fetching

--- components/MovieList.js
+++ components/MovieList.js
@@ -2,10 +2,16 @@ import React from "react";
 
 const MovieList = (props) => {
 	const movies = props.movies;
+	shorten = (text, maxLength) => {
+		if (text && text.length >= maxLength) {
+			return text.substr(0, maxLength) + "...";
+		}
+		return text;
+	};
 	return (
 		<React.Fragment>
 			{movies.map((movie) => (
-				<div className="col-lg-4 col-md-6 mb-4">
+				<div key={movie.id} className="col-lg-4 col-md-6 mb-4">
 					<div className="card h-100">
 						<a href="#">
 							<img className="card-ilmg-top" src={movie.image} alt="" />
@@ -15,7 +21,7 @@ const MovieList = (props) => {
 								<a href="#">{movie.name}</a>
 							</h4>
 							<h5>$24.99</h5>
-							<p className="card-text">{movie.description}</p>
+							<p className="card-text">{shorten(movie.description, 100)}</p>
 						</div>
 						<div className="card-footer">
 							<small className="text-muted">{movie.rating}</small>
