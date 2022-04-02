a34454358c521e61f8fe9c66fc937713a6f36f05
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-04-02 14:52:29 -0300

    Added app container

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,59 @@
-import '../styles/globals.css'
+/* import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
 export default MyApp
+ */
+
+import App from "next/app";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+class MovieApp extends App {
+	static async getInitialProps(appContext) {
+		const appProps = await App.getInitialProps(appContext);
+		return { ...appProps };
+	}
+	render() {
+		const { Component, pageProps } = this.props;
+
+		return (
+			<div>
+				<Head>
+					<title>Home</title>
+					<link
+						href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+						rel="stylesheet"
+						integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+						crossOrigin="anonymous"></link>
+					<script
+						src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+						integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+						crossOrigin="anonymous"></script>
+					<script
+						src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.4/umd/popper.min.js"
+						integrity="sha512-+Tn2V/oN9O/kiaJg/1o5bETqyS35pMDJzkhkf8uvCzxmRox69AsWkSpBSMEGEe4EZp07Nunw712J3Xvh5Tti0w=="
+						crossOrigin="anonymous"
+						referrerPolicy="no-referrer"></script>
+				</Head>
+
+				<Navbar />
+				<div className="base-page">
+					<Component {...pageProps} />
+				</div>
+
+				<Footer />
+				<style jsx>{`
+					.base-page {
+						padding-top: 80px;
+					}
+				`}</style>
+			</div>
+		);
+	}
+}
+
+export default MovieApp;
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,12 @@
 import React, { useState, useEffect } from "react";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Navbar from "../components/Navbar";
+
 import SideMenu from "../components/SideMenu";
 import Carousel from "../components/Carousel";
 import MovieList from "../components/MovieList";
-import Footer from "../components/Footer";
 import { getMovies } from "../actions/index";
 
 export default function Home(props) {
+	// Client Side fetching
 	/* const [movies, setMovies] = useState([]);
 
 	useEffect(() => {
@@ -20,25 +18,6 @@ export default function Home(props) {
 	}, []); */
 	return (
 		<div>
-			<Head>
-				<title>Home</title>
-				<link
-					href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-					rel="stylesheet"
-					integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-					crossOrigin="anonymous"></link>
-				<script
-					src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
-					integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
-					crossOrigin="anonymous"></script>
-				<script
-					src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.4/umd/popper.min.js"
-					integrity="sha512-+Tn2V/oN9O/kiaJg/1o5bETqyS35pMDJzkhkf8uvCzxmRox69AsWkSpBSMEGEe4EZp07Nunw712J3Xvh5Tti0w=="
-					crossOrigin="anonymous"
-					referrerPolicy="no-referrer"></script>
-			</Head>
-
-			<Navbar />
 			<div className="home-page">
 				<div className="container">
 					<div className="row">
@@ -50,18 +29,12 @@ export default function Home(props) {
 							<Carousel />
 
 							<div className="row">
-								<MovieList movies={props.movies} />
+								<MovieList movies={props.movies || []} />
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<Footer />
-			<style jsx>{`
-				.home-page {
-					padding-top: 80px;
-				}
-			`}</style>
 		</div>
 	);
 }
