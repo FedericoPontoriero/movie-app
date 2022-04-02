73186d335abea6038230ed3c5c0392be1dc81b43
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-04-02 15:03:23 -0300

    Added dynamic path to movies

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Link from "next/link";
 const MovieList = (props) => {
 	const movies = props.movies;
 	shorten = (text, maxLength) => {
@@ -13,12 +13,16 @@ const MovieList = (props) => {
 			{movies.map((movie) => (
 				<div key={movie.id} className="col-lg-4 col-md-6 mb-4">
 					<div className="card h-100">
-						<a href="#">
-							<img className="card-ilmg-top" src={movie.image} alt="" />
-						</a>
+						<Link href={`/movies/${movie.id}`}>
+							<a>
+								<img className="card-ilmg-top" src={movie.image} alt="" />
+							</a>
+						</Link>
 						<div className="card-body">
 							<h4 className="card-title">
-								<a href="#">{movie.name}</a>
+								<Link href={`/movies/${movie.id}`}>
+									<a>{movie.name}</a>
+								</Link>
 							</h4>
 							<h5>$24.99</h5>
 							<p className="card-text">{shorten(movie.description, 100)}</p>
