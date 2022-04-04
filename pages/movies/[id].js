82b4e090a86902273f3567f5aa0179fe3b4c7e24
FedericoPontoriero <federicolucaspontoriero@gmail.com>
2022-04-04 14:07:40 -0300

    Added movie by id

--- pages/movies/[id].js
+++ pages/movies/[id].js
@@ -1,13 +1,30 @@
 import { useRouter } from "next/router";
+import { getMovieById } from "../../actions";
 
 const Movie = () => {
 	const router = useRouter();
 	const { id } = router.query;
+	const { movie } = props;
 	return (
 		<div className="container">
-			<h1>Movie with id: {id} </h1>
+			<div className="jumbotron">
+				<h1 className="diplay-4">{movie.name}</h1>
+				<p className="lead"></p>
+				<hr className="my-4" />
+				<p></p>
+				<a className="btn btn-primary btn-lg" role={button}>
+					Learn more
+				</a>
+			</div>
+			<p>description</p>
 		</div>
 	);
+};
+
+Movie.getInitialProps = async () => {
+	const movie = await getMovieById("2");
+
+	return { movie };
 };
 
 export default Movie;
